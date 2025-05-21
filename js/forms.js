5bfdb47b2eeb6b4a02a918c88989d620a1b403a3
nana <anaclaranasc23@gmail.com>
2025-05-21 15:32:15 -0300

    att js

--- js/forms.js
+++ js/forms.js
@@ -4,20 +4,30 @@ function checkInputs(){
     let email = document.querySelector('#email').value
     let telefone = document.querySelector('#telefone').value
     let mensagem = document.querySelector('#mensagem').value
+    usuario = email.substring(0, email.indexOf("@"));
+    dominio = email.substring(email.indexOf("@")+ 1, email.length);
     
     if (!nome || !email || !telefone || !mensagem){
         alert("Preencha todos os campos!")
-       
+        
     }
+    else if(telefone.length != 11 || !parseInt(telefone)){
+     alert   ("telefone inválido")
+    }
+    else if ((usuario.length <1) ||
+            (dominio.length <3) ||
+            (email.indexOf("@")== -1)||
+            (usuario.search("@")!=-1) ||
+            (dominio.search("@")!=-1) ||
+            (usuario.search(" ")!=-1) ||
+            (dominio.search(" ")!=-1) ||
+            (dominio.search(".")==-1) ||
+            (dominio.indexOf(".") <=1)||
+            (dominio.lastIndexOf(".") > dominio.length - 1)) {
+        alert("E-mail inválido");
+        }
     else{
         alert("Mensagem enviada!")
     }
     
 }
-
-
-/*let expressao = '/^(55)?(?:([1-9]{2})?)(\d{4,5})(\d{4})$/';
-    let regex = new RegExp(expressao);
-else if(!regex.test(telefone)){
-       alert("Telefone inválido!")
-    }*/
